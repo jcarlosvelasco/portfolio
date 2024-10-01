eca764932612d198b1657894c429b9f19210ee33
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2024-10-01 20:20:27 +0200

    Vercel redirect fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
+import vercel from "@astrojs/vercel/serverless"
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+
 	i18n: {
 		locales: ["en", "es"],
 		defaultLocale: "en",
@@ -13,4 +16,7 @@ export default defineConfig({
 			prefixDefaultLocale: true,
 		},
 	},
+
+	output: "server",
+	adapter: vercel(),
 })
