5610b488eba625ac991f80471b0cd2056c5d7e96
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2024-10-02 11:36:42 +0200

    New Hero

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,19 +7,21 @@ export const defaultLang = 'en';
 
 export const ui = {
     en: {
-        'nav.heading': 'Software Engineer based in Salamanca, Spain',
+        'nav.heading': 'Software Engineer based in Salamanca, Spain. I\'m passinate about building apps and websites.',
         "nav.about": "About",
         "nav.experience": "Work experience",
         "nav.education": "Education",
         "nav.skills": "Skills",
         "nav.projects": "Projects",
+        "nav.hero": "Hi, I'm Juan Carlos"
     },
     es: {
-        'nav.heading': 'Ingeniero de Software en Salamanca, España',
+        'nav.heading': 'Ingeniero de Software en Salamanca, España. Me apasiona construir aplicaciones y sitios web.',
         "nav.about": "Sobre mí",
         "nav.experience": "Experiencia laboral",
         "nav.education": "Educación",
         "nav.skills": "Habilidades",
         "nav.projects": "Proyectos",
+        "nav.hero": "Hola, soy Juan Carlos"
     },
 } as const;
